a70a54a19100387be9e3cc5ceef8888d2bd1707c
DebugRelease <40682151+Mingxuel@users.noreply.github.com>
2025-12-08 12:30:43 +0800

    Add files via upload

---
+++ MainWindowVM.cs
@@ -0,0 +1,105 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Threading;
+using System;
+using System.Diagnostics;
+
+namespace MyClock
+{
+    public partial class MainWindowVM : ObservableObject
+    {
+        private int pre_minute = -1;
+        private System.Windows.Threading.DispatcherTimer _timer;
+
+        [ObservableProperty]
+        private string hour = "00";
+
+        [ObservableProperty]
+        private string minute = "00";
+
+        [ObservableProperty]
+        private string second = "00";
+
+        [ObservableProperty]
+        private double fontSize = 36;
+
+        [ObservableProperty]
+        private Brush foreground = new SolidColorBrush(Color.FromRgb(255, 128, 0));
+
+        [ObservableProperty]
+        public int targetCount = 0;
+
+        [ObservableProperty]
+        private ObservableCollection<ProcessItem> processItems = new ObservableCollection<ProcessItem>();
+
+        public MainWindowVM()
+        {
+            _timer = new System.Windows.Threading.DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(1000)
+            };
+
+            _timer.Tick += (s, e) =>
+            {
+                Application.Current.Dispatcher.Invoke(() => {
+                    CallTimer();
+                }, DispatcherPriority.Send);
+            };
+
+            _timer.Start();
+        }
+
+        private void CallTimer()
+        {
+            var now = DateTime.Now;
+            Hour = now.Hour.ToString("D2");
+            Minute = now.Minute.ToString("D2");
+            Second = now.Second.ToString("D2");
+
+            DateTime start_time = now.Date.AddHours(09).AddMinutes(14);
+            TimeSpan diff = now - start_time;
+            TargetCount = (int)diff.TotalMinutes;
+            if (pre_minute != TargetCount && TargetCount >= 1 && TargetCount <= 345)
+            {
+                UpdateProcess();
+            }
+            if (pre_minute != TargetCount) pre_minute = TargetCount;
+        }
+
+        public void UpdateProcess()
+        {
+            ProcessItems.Clear();
+            for (int i = 1; i <= 345; i++)
+            {
+                var status = 1;
+                if (i <= TargetCount && (i <= 10 || i >= 343))
+                {
+                    status = 3;
+                    Foreground = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+                }
+                else if (i <= TargetCount && ((i > 15 && i <= 135) || (i > 225)))
+                {
+                    status = 2;
+                    Foreground = Brushes.White;
+                }
+                else
+                {
+                    Foreground = new SolidColorBrush(Color.FromRgb(255, 128, 0));
+                }
+
+                ProcessItems.Add(new ProcessItem
+                {
+                    Id = i + 1, IsSolid = status
+                });
+            }
+        }
+    }
+}
---
+++ ProcessItem.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Windows.Media;
+
+namespace MyClock
+{
+    /// <summary>
+    /// 矩形数据模型（新增IsSolid标识）
+    /// </summary>
+    public class ProcessItem
+    {
+        public int Id { get; set; }
+        public int IsSolid { get; set; } = 1;
+        public double Width { get; set; } = 6;
+        public double Height { get; set; } = 6;
+        public double StrokeThickness { get; set; } = 0.5;
+        public Brush HollowStroke => new SolidColorBrush(Color.FromRgb(20, 20, 20));
+        public Brush HollowFill => new SolidColorBrush(Color.FromRgb(20, 20, 20));
+        public Brush SolidStroke => new SolidColorBrush(Color.FromRgb(255, 128, 0));
+        public Brush SolidFill => new SolidColorBrush(Color.FromRgb(255, 128, 0));
+        public Brush SolidBiddingStroke => Brushes.Red;
+        public Brush SolidBiddingFill => Brushes.Red;
+    }
+}
